d78ded4bb3da69f2971514090cd1caf714c1cccc
lxxue <lxxue@buaa.edu.cn>
2019-11-24 14:52:01 +0100

    modify eval code

--- examples/cinc17/myeval.py
+++ examples/cinc17/myeval.py
@@ -4,16 +4,47 @@ import numpy as np
 import scipy.io as sio
 import scipy.stats as sst
 
-import load
+import sys
+sys.path.append("../../ecg")
+
+import myload as load
 import network
 import util
 
-def mypredict():
-    # ecg = load.load_ecg(record +".mat")
-    preproc = util.load(".")
-    x = preproc.process_x([ecg])
+import pandas as pd
+import numpy as np
 
-    params = json.load(open("config.json"))
+import argparse
+
+from collections import Counter
+
+STEP = 256
+
+def mypredict(args):
+    # ecg = load.load_ecg(record +".mat")
+    if args.data_fname[-3:] == "npy":
+        x_test = np.load(args.data_fname)
+    elif args.data_fname[-3:] == "csv":
+        x_test = pd.read_csv(args.data_fname)
+        x_test.drop(['id'], axis=1)
+        x_test = x_test.fillna(0)
+        x_test = x_test.values
+        np.save(args.data_fname[:-4], x_test)
+
+    print("data loading done")
+    x_test_list = []
+    for i in range(len(x_test)):
+        x_i_end = x_test[i].nonzero()[0][-1] + 1 
+        x_i_end = STEP * int((x_i_end) / STEP) 
+        # print(x_i_end)
+        x_test_list.append(x_test[i][:x_i_end])
+
+
+
+    preproc = util.load(args.prep_fname)
+    x_test = preproc.process_x(x_test_list)
+
+    params = json.load(open(args.config_fname))
     params.update({
         "compile" : False,
         "input_shape": [None, 1],
@@ -21,12 +52,35 @@ def mypredict():
     })
 
     model = network.build_network(**params)
-    model.load_weights('model.hdf5')
+    model.load_weights(args.model_fname)
 
-    probs = model.predict(x)
-    prediction = sst.mode(np.argmax(probs, axis=2).squeeze())[0][0]
-    return preproc.int_to_class[prediction]
+    # print(x_test.shape)
+    probs = model.predict(x_test)
+    preds = np.argmax(probs, axis=2)
+    results = -np.ones((len(x_test),), dtype=np.int64)
+    for i in range(len(x_test)):
+        # print(len(x_test_list[i])//STEP)
+        # print(preds[i, :len(x_test_list[i])//STEP])
+        # print(sst.mode(preds[i, :len(x_test_list[i])//STEP])[0][0])
+
+        results[i] = sst.mode(preds[i, :len(x_test_list[i])//STEP])[0][0]
+    # print(probs.shape)
+    # prediction = sst.mode(np.argmax(probs, axis=2).squeeze())[0][0]
+    print(Counter(results))
+    
+    sample = pd.read_csv(args.sample_fname)
+    sample['y'] = results
+    sample.to_csv(args.submit_fname, index=False)
+    return
 
 if __name__ == '__main__':
-    import sys
-    print predict(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_fname")
+    parser.add_argument("prep_fname")
+    parser.add_argument("model_fname")
+    parser.add_argument("config_fname")
+    parser.add_argument("sample_fname")
+    parser.add_argument("submit_fname")
+    args = parser.parse_args()
+    print(args)
+    mypredict(args)
---
+++ examples/cinc17/sh_myeval.sh
@@ -0,0 +1,2 @@
+# python myeval.py data/X_test.csv ../../saved/mycinc17/1574595753-617/ ../../saved/mycinc17/1574595753-617/0.433-0.850-027-0.307-0.891.hdf5 myconfig.json
+python myeval.py data/X_test.npy ../../saved/mycinc17/1574595753-617/ ../../saved/mycinc17/1574595753-617/0.433-0.850-027-0.307-0.891.hdf5 myconfig.json data/sample.csv ../../saved/mycinc17/1574595753-617/submit.csv
